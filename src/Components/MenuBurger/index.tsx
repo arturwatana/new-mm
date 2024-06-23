212a39b79b827758d203ec1fb8c03f60d68b61dc
Artur Watanabe <arturwatanabe@gmail.com>
2024-06-23 15:16:24 -0300

    fix: scroll

--- src/Components/MenuBurger/index.tsx
+++ src/Components/MenuBurger/index.tsx
@@ -4,6 +4,7 @@ import Translator from "../Translator"
 import BrazilFlag from "../../assets/brazil-flag-icon.svg"
 import USFlag from "../../assets/united-states-flag-icon.svg"
 import { Img } from "@chakra-ui/react"
+import { useEffect } from "react"
 
 type BurgerProps = {
     isActive: boolean
@@ -21,6 +22,7 @@ export default function MenuBurger({ isActive, setRenderBurger }: BurgerProps) {
             i18n.changeLanguage(language)
             setRenderBurger(false)
         }
+       
         const menuItens: MenuItemProps[] = [{ name: Translator("burger.home"), href: "#firstSection" }, { name: Translator("burger.aboutUs"), href: "#secondSection" }, { name: Translator("burger.atlantis"), href: "#thirdSection" }, { name: Translator("burger.reviews"), href: "#fourthSection" }, { name: Translator("burger.contactUs"), href: "#fifthSection" }]
         const variants = {
             render: {
@@ -46,11 +48,23 @@ export default function MenuBurger({ isActive, setRenderBurger }: BurgerProps) {
             const element = document.getElementById(id)
             element?.scrollIntoView({ behavior: 'smooth' });
         }
+
+        useEffect(() => {
+            if (isActive) {
+                document.body.classList.add('no-scroll');
+            } else {
+                document.body.classList.remove('no-scroll');
+            }
+            return () => {
+                document.body.classList.remove('no-scroll');
+            };
+        }, [isActive]);
+        
         return (
             <>
                 <motion.div onClick={() => setRenderBurger(false)} style={{ width: "100%", height: "100%", background: "rgb(34, 2, 0, 0.4)", position: "absolute", zIndex: 900 }}>
                 </motion.div>
-                <motion.div initial={{ x: 100, opacity: 0 }} animate={isActive ? "render" : "hide"} variants={variants} style={{ position: "fixed", zIndex: 999, background: "#f6f6f6", height: "100%", width: "330px", display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "start", left: "30%", padding: "85px 35px " }}>
+                <motion.div initial={{ x: 100, opacity: 0 }} animate={isActive ? "render" : "hide"} variants={variants} style={{ position: "fixed", zIndex: 999, background: "#f6f6f6", height: "100vh", width: "330px", display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "start", left: "30%", padding: "85px 35px " }}>
                     <motion.ul style={{ gap: "20px", display: "flex", flexDirection: "column", listStyle: "none", textAlign: "left", fontSize: "20px" }}>
                         {menuItens.map((item: MenuItemProps, index: any) => <motion.a className={item.href.replace("#", "")} onClick={(e) => { scrollToTarget(e); setRenderBurger(false) }} key={index} initial={{ x: 200, opacity: 0 }} variants={variants} custom={index} animate={isActive ? "renderLi" : ""} style={{ cursor: "pointer" }}>{item.name}</motion.a>)}
                     </motion.ul>
